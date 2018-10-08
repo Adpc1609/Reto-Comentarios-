8fddde0d932673db7ec2a2da6535967d5e865272
Adriana Perez <adpc1609@gmail.com>
2018-10-08 19:38:23 -0300

    funciones de escribir y leer

---
+++ src/scripts/app.js
@@ -0,0 +1,48 @@
+firebase.initializeApp({
+    apiKey:  "AIzaSyBFg2L002eMUCHYDnZiCrpl3pcQTjAJT20",
+    authDomain: "reto-comentario.firebaseapp.com",
+    projectId: "reto-comentario"
+  });
+  
+  // Initialize Cloud Firestore through Firebase
+let db = firebase.firestore();
+
+function comentar(){
+let comment = document.getElementById('comment').value;
+  db.collection("users").add({
+   mensaje: comment
+})
+.then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+    let comment = document.getElementById('comment').value= "";
+
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
+});
+}
+
+//leer Documentos 
+let cardTexto = document.getElementById("card");
+db.collection("users").onSnapshot((querySnapshot) => {
+ cardTexto.innerHTML = "";
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data().mensaje}`);
+        cardTexto.innerHTML += `
+        <div class="card">
+        <div class="card-body">
+        ${doc.data().mensaje}
+        </div>
+        </div>
+        <button onclick="eliminar()">eliminar</button>
+        `
+    });
+});
+
+function eliminar(){
+db.collection("cities").doc("DC").delete().then(function() {
+    console.log("Document successfully deleted!");
+}).catch(function(error) {
+    console.error("Error removing document: ", error);
+});
+}
